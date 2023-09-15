e94ff815a05fc7f14cbe027282b370d8119714a2
KibetAmon <kibetngetich805@gmail.com>
2023-09-15 11:40:21 +0300

    Added UPDATE and DELETE functionality

--- RestAPI/person/views.py
+++ RestAPI/person/views.py
@@ -26,7 +26,7 @@ class PersonAPIView(APIView):
             serializer = personSerializer(data, many=True)
             return Response(serializer.data)
 
-    # A function to create a person
+    # A function to CREATE a person
     def post(self, request, format=None):
         data = request.data
         serializer = personSerializer(data=data)
@@ -43,3 +43,35 @@ class PersonAPIView(APIView):
             'message': 'Person added successfully',
             'data': serializer.data
         }
+
+        return response
+
+    # UPDATE a person
+    def put(self, request, pk=None, format=None):
+
+        # Get the person to update
+        person_to_update = person.objects.get(pk=pk)
+
+        # Pass the instance to update to the serializer, and the data and also partial to the serializer
+        # Passing partial will allow us to update without passing the entire person object
+
+        serializer = personSerializer(instance=person_to_update, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response = Response()
+        response.data = {
+            'message': 'Person updated successfully',
+            'data': serializer.data
+        }
+
+        return response
+
+    # DELETE a person
+    def delete(self, request, pk, format=None):
+        person_to_delete = person.objects.get(pk=pk)
+
+        # delete the person
+        person_to_delete.delete()
+        return Response({
+            'message':'Person deleted successfully'
+        })
